2c8c968ad9009ab5a5d63c6dc675f030f7670a86
ArtemZayarny <artyom.zayarny@gmail.com>
2025-05-21 14:19:11 +0300

    Get current geolocation

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+//Get current geo position
+function onSuccess(position) {
+  const { latitude, longitude } = position.coords;
+  console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+}
+function onError() {
+  alert("Could not get your location");
+}
+console.log(navigator);
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
