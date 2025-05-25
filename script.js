4d6ecd2e5c91a8d1614f33352d2f12235b573698
ArtemZayarny <artyom.zayarny@gmail.com>
2025-05-25 09:58:09 +0300

    Render form inputs

--- script.js
+++ script.js
@@ -11,12 +11,14 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
+
 //Get current geo position
 function onSuccess(position) {
   const { latitude, longitude } = position.coords;
   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-  const map = L.map('map').setView([latitude, longitude], 10);
+  map = L.map('map').setView([latitude, longitude], 10);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -26,10 +28,30 @@ function onSuccess(position) {
       .bindPopup('A pretty CSS popup.<br> Easily customizable.')
       .openPopup();
 
-  //Add click event for map
-  map.on('click', (mapEvent) => {
-    console.log(mapEvent);
-    const { lat, lng } = mapEvent.latlng;
+    map.on('click', (mapE) => {
+      console.log('mapE', mapE)
+      mapEvent = mapE;
+      console.log(mapEvent)
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    })
+
+
+}
+function onError() {
+  alert("Could not get your location");
+}
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  //Clear input fields
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+
+  //Display marker
+  const { lat, lng } = mapEvent.latlng;
     L.marker([lat,lng]).addTo(map).bindPopup(L.popup({
       maxWidth:250,
       minWidth:100,
@@ -39,12 +61,10 @@ function onSuccess(position) {
     }))
         .setPopupContent('Workout')
         .openPopup();
-  })
-}
-function onError() {
-  alert("Could not get your location");
-}
-console.log(navigator);
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
+});
 
-
+//Change type
+inputType.addEventListener('change', (e)=>{
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
