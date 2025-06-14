19aa1a48a8d88a427ddbbf39a1484d9f6a542189
ArtemZayarny <artyom.zayarny@gmail.com>
2025-06-14 09:19:35 +0300

    Move map to the marker

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
@@ -18,6 +19,7 @@ class App {
     form.addEventListener('submit', this._newWorkout.bind(this));
     //Change type
     inputType.addEventListener('change', this._toggleElevationField)
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _onErrorGeo(){
@@ -26,13 +28,13 @@ class App {
 
   _getPosition() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), this.onErrorGeo);
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), this._onErrorGeo);
     }
   }
 
   _loadMap(position){
     const { latitude, longitude } = position.coords;
-    this.#map = L.map('map').setView([latitude, longitude], 10);
+    this.#map = L.map('map').setView([latitude, longitude], this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -106,7 +108,6 @@ class App {
     this._hideForm();
   }
 
-
   _renderWorkoutMarker(workout) {
       L.marker(workout.coords)
           .addTo(this.#map)
@@ -166,12 +167,28 @@ class App {
         </li>`;
     form.insertAdjacentHTML('afterend',html);
   }
-}
 
+  _moveToPopup(e){
+    const workoutEl = e.target.closest('.workout');
+    if(!workoutEl) return;
+    const workout = this.#workouts.find(w=>w.id === workoutEl.dataset.id);
+    this.#map.setView(workout.coords,this.#mapZoomLevel,
+        {
+          animate:true,
+          pan: {
+            duration:1
+          }
+        });
+    // using  public interface
+    workout.click()
+  }
+}
 
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  clicks = 0;
+
   constructor(coords, distance, duration) {
       this.coords = coords; // [lat,lng]
       this.distance = distance; // in km
@@ -180,6 +197,9 @@ class Workout {
   _setDescription(){
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+  }
+  click(){
+    this.clicks++;
   }
 }
 
@@ -197,6 +217,7 @@ class Running extends Workout {
     return this.pace;
   }
 }
+
 class Cycling extends Workout {
   type = 'cycling'
   constructor(coords, distance, duration,elevationGain, speed) {
